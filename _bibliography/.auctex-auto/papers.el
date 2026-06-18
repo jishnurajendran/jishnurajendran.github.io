22b6d5c836e951f7e0ebdc1a213c1da0c0a66291
Jishnu <rjishnu55@gmail.com>
2026-06-18 22:45:57 +0530

    elsp for tex

---
+++ _bibliography/.auctex-auto/papers.el
@@ -0,0 +1,15 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "papers"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Giannelli2022"
+    "Rajendran_2018"
+    "Rajendran2018"
+    "Ridolfo2021"
+    "Pope2019"
+    "Macri2022"
+    "rajendran_2024"))
+ '(or :bibtex :latex))
+
